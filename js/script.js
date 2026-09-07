9684d862b3eb1e1eb1fba4d1ca1cfdf8fc4eff49
RosealievPieかれし♪ <rosalisagustin@gmail.com>
2026-09-08 06:34:38 +0700

    Remove favorite boxes click event handler
    Removed favorite boxes functionality from script.

--- js/script.js
+++ js/script.js
@@ -33,13 +33,3 @@ if (contactTrigger && navContactBox) {
 
     contactObserver.observe(contactTrigger);
 }
-
-//  favorite
-const favoriteBoxes = document.querySelectorAll('.bg-opacity-10');
-
-favoriteBoxes.forEach(box => {
-    box.addEventListener('click', () => {
-        box.classList.toggle('bg-[#cb879c]/30'); // Berubah warna saat diklik
-        alert("Yay! You found one of my favorite things! ✨");
-    });
-});
